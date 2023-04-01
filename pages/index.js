0499e4871de9dbba0ee5562a9c6d78835de937f1
Ira Bibaeva <irabbv@yandex.ru>
2023-04-01 23:00:40 +0400

    feat/popupJs: open and close & hotfix/pixelperfect

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let editBtn = document.querySelector('.profile__edit-button');
+
+let popup = document.querySelector('.popup');
+let formElement = popup.querySelector('.form');
+let formName = popup.querySelector('.form__name');
+let formDescription = popup.querySelector('.form__description');
+let closeBtn = popup.querySelector('.form__close-icon');
+let submitBtn = popup.querySelector('.form__submit-button');
+
+formName.value = profileName.textContent;
+formDescription.value = profileDescription.textContent;
+
+function openPopup() {
+    popup.classList.remove('popup_hidden');
+}
+editBtn.addEventListener('click', openPopup);
+
+function closePopup(event) {
+    const isCloseBtn = event.target.classList.contains('popup');
+    const isOverlay = event.target.classList.contains('form__close-icon');
+    if (isCloseBtn || isOverlay) {
+        popup.classList.add('popup_hidden');
+    }
+}
+popup.addEventListener('click', closePopup);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    return (
+        profileDescription.value = formDescription.textContent,
+        profileName.value = formName.textContent
+      );
+    //popup.classList.add('popup_hidden');
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+submitBtn.addEventListener('click', handleFormSubmit);
