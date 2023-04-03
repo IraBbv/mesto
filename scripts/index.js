6a1733ccd0426322e339df675d1f3108c6ef89ea
Ira Bibaeva <irabbv@yandex.ru>
2023-04-03 20:21:54 +0400

    hotfix/everything v.1

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+// Обозначаем элементы
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let editBtn = document.querySelector('.profile__edit-button');
+
+let popup = document.querySelector('.popup');
+let formElement = popup.querySelector('.form');
+let closeBtn = popup.querySelector('.form__close-icon');
+let formName = popup.querySelector('.form__input_type_name');
+let formDescription = popup.querySelector('.form__input_type_description');
+
+// Функция открытия попапа, содержимое профиля будет в форме
+function openPopup() {
+    popup.classList.add('popup_open');
+    formName.value = profileName.textContent;
+    formDescription.value = profileDescription.textContent;
+}
+
+//Функция закрытия попапа
+function closePopup() {
+    popup.classList.remove('popup_open');
+}
+
+// Функция сохранения введённой информации
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    
+    profileName.textContent = formName.value;
+    profileDescription.textContent = formDescription.value;
+    
+    closePopup();
+}
+
+// Слушатели событий
+editBtn.addEventListener('click', openPopup);
+closeBtn.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
