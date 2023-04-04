3f19b4a358d06a238d150af2fb2aa2866b5b9cdf
Ira Bibaeva <irabbv@yandex.ru>
2023-04-04 18:44:12 +0400

    hotfix/everithing v2.0

--- scripts/index.js
+++ scripts/index.js
@@ -11,14 +11,14 @@ let formDescription = popup.querySelector('.form__input_type_description');
 
 // Функция открытия попапа, содержимое профиля будет в форме
 function openPopup() {
-    popup.classList.add('popup_open');
+    popup.classList.add('popup_opened');
     formName.value = profileName.textContent;
     formDescription.value = profileDescription.textContent;
 }
 
 //Функция закрытия попапа
 function closePopup() {
-    popup.classList.remove('popup_open');
+    popup.classList.remove('popup_opened');
 }
 
 // Функция сохранения введённой информации
